1f318a9193d160c57ae4e4440678d1556c57f02c
mmanrus <mmanrusiana@gmail.com>
2025-06-14 15:07:06 +0800

    moved files to backend

--- autochecker/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Classroom, Activity, Submission, CustomUser
-# Register your models here.
-
-admin.site.register(Activity)
-admin.site.register(Submission)
-admin.site.register(CustomUser)
-admin.site.register(Classroom)
--- autochecker/validators.py
+++
@@ -1,6 +0,0 @@
-import re
-from django.core.exceptions import ValidationError
-
-def validate_custom_slug(value):
-    if not re.match(r'^[a-zA-Z0-9-_\/]+$', value):  # Allows letters, numbers, _, -, and /
-        raise ValidationError('Only letters, numbers, underscores (_), hyphens (-), and slashes (/) are allowed.')
